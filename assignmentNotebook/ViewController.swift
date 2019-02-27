0addc392ec01ed937126165811ddabcd816b81af
Julio Rivera <jrivera0325@jh-mba158-16.local>
2019-02-27 11:19:20 -0600

    added assignment class

--- assignmentNotebook/ViewController.swift
+++ assignmentNotebook/ViewController.swift
@@ -4,17 +4,30 @@
 //
 //  Created by Julio Rivera on 2/26/19.
 //  Copyright © 2019 John Hersey High School. All rights reserved.
-//
+// video 5 min 4.44
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var newAssignmentTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+    }
 
+
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+        cell.textLabel?.text = "Hello"
+        return cell
     }
 
 
---
+++ assignmentNotebook/assignment.swift
@@ -0,0 +1,15 @@
+//
+//  assignment.swift
+//  assignmentNotebook
+//
+//  Created by Julio Rivera on 2/27/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//
+
+import Foundation
+class Assignment {
+    var name: String
+    init(name:String) {
+        self.name = name
+    }
+}
