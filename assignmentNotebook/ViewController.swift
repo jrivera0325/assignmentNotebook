8e4cd61d4c0d06f76533a8ea783f0a27cd7fe8e1
Julio Rivera <jrivera0325@jh-mba158-16.local>
2019-02-27 11:36:40 -0600

    MVP done

--- assignmentNotebook/ViewController.swift
+++ assignmentNotebook/ViewController.swift
@@ -4,29 +4,37 @@
 //
 //  Created by Julio Rivera on 2/26/19.
 //  Copyright © 2019 John Hersey High School. All rights reserved.
-// video 5 min 4.44
+//
 
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var newAssignmentTextField: UITextField!
+    var assignment: [Assignment] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         tableView.dataSource = self
+        
+        var assignment1 = Assignment(name: "Math Worksheet")
+        var assignment2 = Assignment(name: "Huck Finn Essay")
+        var assignment3 = Assignment(name: "Assignment Notebook App")
+        assignment = [assignment1, assignment2, assignment3]
+        
     }
-
-
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return assignment.count
     }
 
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
-        cell.textLabel?.text = "Hello"
+        let currentAssigmentName = assignment[indexPath.row].name
+        cell.textLabel?.text = currentAssigmentName
         return cell
     }
 
